e1b0ff99463b0086220ad3ec38aded4dce68508c
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-11 17:54:37 -0300

    guardando

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,3 +1,4 @@
+import { marcas, planes } from "../constants"
 import { useCotizador } from "../hooks/useCotizador"
 
 export const Cotizacion = () => {
@@ -11,14 +12,15 @@ export const Cotizacion = () => {
 
   return (
     <div className="mt-5 text-center bg-gray-100 p-2 shadow rounded">
+      <h2 className="uppercase text-3xl text-gray-700 font-black mb-5">Resumen</h2>
       <p className="mb-2 text-lg font-bold text-gray-400">Marca del Auto: {""}
-        <span className="text-black">{marca}</span>
+        <span className="text-black">{marcas[marca].nombre}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Año del Auto: {""}
         <span className="text-black">{year}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Plan: {""}
-        <span className="text-black"></span>
+        <span className="text-black">{planes[plan].nombre}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Monto a Pagar: {""}
         <span className="text-black">{cotizacion}</span>
