a871defccd2242772e638d5eb251266b30539a55
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-12 10:19:04 -0300

    Añadiendo más Información al Resultado

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -4,7 +4,10 @@ import { useCotizador } from "../hooks/useCotizador"
 export const Cotizacion = () => {
 
   const { datos, cotizacion } = useCotizador()
-  const { marca, year } = datos
+  const { marca, year, plan } = datos
+
+  const [ nombreMarca ] = marcas.filter(m => m.id === +marca)
+  const [ nombrePlan ] = planes.filter(p => p.id === +plan)
 
   if (!cotizacion) {
     return null
@@ -14,13 +17,13 @@ export const Cotizacion = () => {
     <div className="mt-5 text-center bg-gray-100 p-2 shadow rounded">
       <h2 className="uppercase text-3xl text-gray-700 font-black mb-5">Resumen</h2>
       <p className="mb-2 text-lg font-bold text-gray-400">Marca del Auto: {""}
-        <span className="text-black">{marcas[marca].nombre}</span>
+        <span className="text-black">{nombreMarca.nombre}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Año del Auto: {""}
         <span className="text-black">{year}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Plan: {""}
-        <span className="text-black">{planes[plan].nombre}</span>
+        <span className="text-black">{nombrePlan.nombre}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Monto a Pagar: {""}
         <span className="text-black">{cotizacion}</span>
