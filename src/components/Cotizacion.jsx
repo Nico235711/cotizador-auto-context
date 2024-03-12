421e6e5e967955ae1d4f235d618dce9106a142b1
Nicolás Lopez Vidueiros <139637937+Nico235711@users.noreply.github.com>
2024-03-12 10:31:54 -0300

    useCallback y useRef

--- src/components/Cotizacion.jsx
+++ src/components/Cotizacion.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useMemo, useRef } from "react"
 import { marcas, planes } from "../constants"
 import { useCotizador } from "../hooks/useCotizador"
 
@@ -5,9 +6,16 @@ export const Cotizacion = () => {
 
   const { datos, cotizacion } = useCotizador()
   const { marca, year, plan } = datos
+  const yearRef = useRef(year)
 
-  const [ nombreMarca ] = marcas.filter(m => m.id === +marca)
-  const [ nombrePlan ] = planes.filter(p => p.id === +plan)
+  // evitando los re-renders de react con useCallback
+  const [ nombreMarca ] = useCallback(
+    marcas.filter(m => m.id === +marca), [cotizacion]
+  )
+
+  const [ nombrePlan ] = useCallback(
+    planes.filter(p => p.id === +plan), [cotizacion]
+  )
 
   if (!cotizacion) {
     return null
@@ -20,7 +28,7 @@ export const Cotizacion = () => {
         <span className="text-black">{nombreMarca.nombre}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Año del Auto: {""}
-        <span className="text-black">{year}</span>
+        <span className="text-black">{yearRef.current}</span>
       </p>
       <p className="mb-2 text-lg font-bold text-gray-400">Plan: {""}
         <span className="text-black">{nombrePlan.nombre}</span>
